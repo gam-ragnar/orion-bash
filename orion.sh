c7e6a0d8d1d4f1772d946401fcc8edd5292bc58a
tim <tim.anyasi@gmail.com>
2015-05-25 13:50:10 -0400

    minimally usable

--- orion.sh
+++ orion.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 # Orion automated script
 #save arguements in array 
-#
-#
-
 args=("$@")
-
-getAdmin
-getDist
-
-
 
 # commands
 # dl for git clone distribution
 # usage orion dl distName
-# 
 
+function meteorStart {
+  echo "start meteor"
+  meteor 
+}
+#Grab admin theme
+#orion admin bootstrap
 function getAdmin {
   if [ ${args[0]} = "admin" ]; then 
     echo "only admim theme will be added ${args[1]}"
@@ -25,11 +22,11 @@ function getAdmin {
     echo "meteor remove insecure autopublish"
     meteor remove insecure autopublish
     meteor add orionjs:core twbs:bootstrap orionjs:bootstrap
-    echo "start meteor"
-    meteor 
+    meteorStart  
   fi
 }
-
+# grab Orion Distribution
+# orion dl blog
 function getDist {
   if [ ${args[0]} = "dl" ]; then 
   echo "grabbing your" ${args[1]}
@@ -41,33 +38,12 @@ function getDist {
   `sudo mv blog/* ../`
   cd ..
   rm -rf examples
-  cd  ..
+  cd  ${args[1]}
+  meteorStart 
   fi
 }
 
-# else
-#     echo "use a admin or dl "
-#function quit {
-#                exit
-#            }
-# function hello {
-#      echo Hello!
-#  }
-#   hello
-#   quit
-#   echo foo 
 
-# listDist = {
-#   bootstrap:'twbs:bootstrap orionjs:bootstrap',
-#   materialize:'materialize:materialize orionjs:materialize',
-#   foundation: 'rwatts:orionjs-foundation ewall:foundation' ,
-#   blog: getBlog()
+getAdmin
+getDist
 
-# }
-
-# s3 = ['fortawesome:fontawesome',  'orionjs:filesystem', 'orionjs:s3',  'orionjs:file-attribute ',  'orionjs:relationships', 'reywood:publish-composite', 'dburles:collection-helpers'];
-
-# foundationExtras= ();
-# getBlog = function(){
-#   return `git clone https://github.com/orionjs/examples.git`
-# }
