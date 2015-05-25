d437a70bfd08a651c725cef32000751b62ea87a5
tim <tim.anyasi@gmail.com>
2015-05-25 15:49:43 -0400

    case added

--- orion.sh
+++ orion.sh
@@ -1,49 +1,69 @@
 #!/bin/bash
-# Orion automated script
-#save arguements in array 
+# Orion automated script that allows you to install either admin or full distribution from shell
+
+#save passed arguements in array 
 args=("$@")
 
-# commands
-# dl for git clone distribution
-# usage orion dl distName
-
-function meteorStart {
-  echo "start meteor"
-  meteor 
+#function that checks chosen distribution exist called further down
+function checkDist {
+  case ${args[1]} in 
+    blog) echo "grabbing your" ${args[1]} 
+    mkdir ${args[1]}
+    cd ${args[1]}
+    `git clone https://github.com/orionjs/examples.git`
+    cd examples
+    shopt -s dotglob
+    `sudo mv blog/* ../`
+    cd ..
+    rm -rf examples
+    echo "cd ${args[1]}";;
+    *) echo "please enter a valid distribution";;
+  esac
 }
+# setup meteor called in checkAdmin
+function meteorSetup {
+  meteor create ${args[2]}
+  echo "cd  ${args[2]}"
+  cd ${args[2]}
+  echo "meteor remove insecure autopublish"
+  meteor remove insecure autopublish
+  meteor add orionjs:core
+  }
+#function that checks chosen admin theme exist called further down
+function checkAdmin {
+  case ${args[1]} in 
+    bootstrap) echo "setting up Bootstrap theme"
+    meteorSetup
+    meteor add twbs:bootstrap orionjs:bootstrap
+    echo "cd ${args[2]}";;
+    foundation) echo "setting up foundation theme"
+    meteorSetup
+    meteor add ewall:foundation rwatts:orionjs-foundation
+    echo "cd ${args[2]}";;
+    materialize) echo "setting up materialize theme"
+    meteorSetup
+    meteor add materialize:materialize orionjs:materialize
+    echo "cd ${args[2]}";;
+    *) echo "please enter a valid distribution";;
+  esac
+}
+
 #Grab admin theme
-#orion admin bootstrap
+# i.e orion.sh admin bootstrap AppName 
 function getAdmin {
   if [ ${args[0]} = "admin" ]; then 
-    echo "only admim theme will be added ${args[1]}"
-    meteor create ${args[1]}
-    echo "cd into" ${args[1]}
-    cd ${args[1]}
-    echo "meteor remove insecure autopublish"
-    meteor remove insecure autopublish
-    meteor add orionjs:core twbs:bootstrap orionjs:bootstrap
-    meteorStart  
+    checkAdmin
   fi
 }
-# grab Orion Distribution
-# orion dl blog
+# # grab Orion Distribution
+# # orion.sh dl DistributionName
 function getDist {
   if [ ${args[0]} = "dl" ]; then 
-  echo "grabbing your" ${args[1]}
-  mkdir ${args[1]}
-  cd ${args[1]}
-  `git clone https://github.com/orionjs/examples.git`
-  cd examples
-  shopt -s dotglob
-  `sudo mv blog/* ../`
-  cd ..
-  rm -rf examples
-  cd  ${args[1]}
-  meteorStart 
+  checkDist
   fi
 }
 
-
+# instantiate bash
 getAdmin
 getDist
 
